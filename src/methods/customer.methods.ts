a376715af7ce9897b2bcd6c61f3c4a7a3eae0d70
alinapauliina <71494779+alinapauliina@users.noreply.github.com>
2025-09-02 15:02:09 +0300

    Add update customer

--- src/methods/customer.methods.ts
+++ src/methods/customer.methods.ts
@@ -1,6 +1,6 @@
 import { Helpers } from '../helpers';
 import { FennoaApiClient } from '../index';
-import { INewCustomer } from '../interfaces/customer.interfaces';
+import { INewCustomer, IUpdateCustomer } from '../interfaces/customer.interfaces';
 import { Methods } from '../methods';
 
 export class CustomerMethods extends Methods {
@@ -18,20 +18,9 @@ export class CustomerMethods extends Methods {
     return await super.getById(id);
   }
 
-  /** Create a new customer. Returns id of the newly created customer. */
-  async createCustomer(customer: INewCustomer): Promise<number> {
-    const formData = Helpers.convertJsToFormData(customer);
-
-    const result = await super.request('POST', 'add', formData);
-
-    return result.id as number;
-  }
-
-  /** Create a new customer using FORM DATA. Returns id of the newly created customer. */
-  async createCustomerByFormData(formData: FormData): Promise<number> {
-    const result = await super.request('POST', 'add', formData);
-
-    return result.id as number;
+  /** Get customer by customer number. E.g. /customer_api/get/customer_no/F123. */
+  async getByCustomerNo(customerNo: string): Promise<any> {
+    return await super.request('GET', `get/customer_no/${customerNo}`);
   }
 
   /** Create a new customer. Returns id of the newly created customer. */
@@ -46,6 +35,22 @@ export class CustomerMethods extends Methods {
   /** Create a new customer using FORM DATA. Returns id of the newly created customer. */
   async createCustomerByFormData(formData: FormData): Promise<number> {
     const result = await super.request('POST', 'add', formData);
+
+    return result.id as number;
+  }
+
+  /** Update customer. Returns id of the updated customer. */
+  async updateCustomer(customerNo: string, customer: IUpdateCustomer): Promise<number> {
+    const formData = Helpers.convertJsToFormData(customer);
+
+    const result = await super.request('PUT', customerNo, formData);
+
+    return result.id as number;
+  }
+
+  /** Update customer using FORM DATA. Returns id of the updated customer. */
+  async updateCustomerByFormData(customerNo: string, formData: FormData): Promise<number> {
+    const result = await super.request('PUT', customerNo, formData);
 
     return result.id as number;
   }
