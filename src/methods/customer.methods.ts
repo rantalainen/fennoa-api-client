ae991caa56634f2caa7acea3e41ed5b63101ea65
alinapauliina <71494779+alinapauliina@users.noreply.github.com>
2025-09-03 14:29:53 +0300

    Add customer methods (#2)
    * Add customer interfaces and methods for creating and updating customers

--- src/methods/customer.methods.ts
+++ src/methods/customer.methods.ts
@@ -1,4 +1,6 @@
+import { Helpers } from '../helpers';
 import { FennoaApiClient } from '../index';
+import { INewCustomer, IUpdateCustomer } from '../interfaces/customer.interfaces';
 import { Methods } from '../methods';
 
 export class CustomerMethods extends Methods {
@@ -14,5 +16,49 @@ export class CustomerMethods extends Methods {
   /** Get all the information about single customer. */
   async getById(id: number): Promise<any> {
     return await super.getById(id);
+  }
+
+  /** Get customer by customer number. E.g. /customer_api/get/customer_no/F123. */
+  async getByCustomerNo(customerNo: string): Promise<any> {
+    return await super.request('GET', `get/customer_no/${customerNo}`);
+  }
+
+  /** Create a new customer. Returns id of the newly created customer. */
+  async createCustomer(customer: INewCustomer): Promise<string> {
+    const formData = Helpers.convertJsToFormData(customer);
+
+    const result = await super.request('POST', 'add', formData);
+
+    if (result?.status !== 'ok' || !result?.data?.Customer) {
+      throw new Error('Fennoa error: Failed to create customer');
+    }
+
+    return result.data.Customer.id;
+  }
+
+  /** Create a new customer using FORM DATA. Returns id of the newly created customer. */
+  async createCustomerByFormData(formData: FormData): Promise<number> {
+    const result = await super.request('POST', 'add', formData);
+
+    if (result?.status !== 'ok' || !result?.data?.Customer) {
+      throw new Error('Fennoa error: Failed to create customer');
+    }
+
+    return result.data.Customer.id;
+  }
+
+  /** Update customer. Returns id of the updated customer. */
+  async updateCustomer(customerNo: string, customer: IUpdateCustomer): Promise<string> {
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+
+    const result = await super.request('PUT', customerNo, JSON.stringify(customer), undefined, headers);
+
+    if (result?.status !== 'ok' || !result?.data?.id) {
+      throw new Error('Fennoa error: Failed to update customer');
+    }
+
+    return result.data.id;
   }
 }
